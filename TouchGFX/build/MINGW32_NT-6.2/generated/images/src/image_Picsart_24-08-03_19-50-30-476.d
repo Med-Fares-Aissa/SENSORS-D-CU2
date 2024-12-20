eaa7ae259da873024528d328d16493425ca25e52
Fares <med.fares.aissa@gmail.com>
2024-12-20 01:26:50 +0100

    Add binary file for the project

--- TouchGFX/build/MINGW32_NT-6.2/generated/images/src/image_Picsart_24-08-03_19-50-30-476.d
+++
@@ -1,5 +0,0 @@
-build/MINGW32_NT-6.2/generated/images/src/image_Picsart_24-08-03_19-50-30-476.o: \
- generated/images/src/image_Picsart_24-08-03_19-50-30-476.cpp \
- ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-
-../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
